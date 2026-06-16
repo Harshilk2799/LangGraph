d95c827b78e6a1ca30ad93054783262c0d6e40cf
Harshil Khatri <khatriharshil26@gmail.com>
2026-06-16 09:56:29 +0530

    Implementation of inspecting checkpoint state in LangGraph.

---
+++ 5_persistence/6.inspecting_checkpoint_state.py
@@ -0,0 +1,63 @@
+from typing import List, Annotated, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_google_genai import ChatGoogleGenerativeAI
+from decouple import config
+
+chat_model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=config("GOOGLE_GEMINI_API_KEY"))
+
+# Define State
+class ChatState(TypedDict):
+    messages: Annotated[list, add_messages]
+
+def chatbot(state: ChatState)-> ChatState:
+    response = chat_model.invoke(state["messages"])
+    return {"messages": [response]}
+
+graph_builder = StateGraph(ChatState)
+graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+
+# Context manager handles connection lifecycle
+with SqliteSaver.from_conn_string("./my_checkpoints.db") as checkpointer:
+
+    workflow = graph_builder.compile(checkpointer=checkpointer)
+
+    config = {"configurable": {"thread_id": "harshil-session-001"}}
+
+    while True:
+        user_input = input("You: ")
+
+        if user_input in ["exit", "bye", "quit"]:
+            print("Bot: Goodbye!")
+            break 
+
+        result = workflow.invoke(
+            {
+                "messages": [
+                    {"role": "user", "content": user_input}
+                ]
+            },
+            config=config
+        )
+        print("AI: ", result["messages"][-1].content)
+
+
+    # Get the latest snapshot
+    snapshot = workflow.get_state(config)
+
+    print(snapshot.values)          # full state dict (all messages)
+    print(snapshot.next)
+    print(snapshot.metadata)
+    print(snapshot.config)
+
+    # Walk through ALL historical checkpoints
+    for checkpoint in workflow.get_state_history(config):
+        print(f"Step {checkpoint.metadata['step']}:")
+        print(f"  next  → {checkpoint.next}")
+        print(f"  msgs  → {len(checkpoint.values['messages'])} messages")
+        print()
